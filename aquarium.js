affa1becaa2f5bd166a5e5374eea74307ef9eb7e
abraham-kun <69225679+abraham-kun@users.noreply.github.com>
2020-09-10 11:51:32 +1000

    Update aquarium.js

--- aquarium.js
+++ aquarium.js
@@ -1,22 +1,29 @@
 window.onload=function() {
-                lengthObj = document.getElementById('txtLength');
-                girthObj = document.getElementById('txtGirth');
-                weightObj = document.getElementById('tdWeight');
-                document.getElementById('btnReset').onclick = resetInputs;
-                document.getElementById('btnCalc').onclick = calcWeight;
+    lengthObj = document.getElementById('txtLength');
+    heightObj = document.getElementById('txtHeight');
+    widthObj = document.getElementById('txtWidth');
+	costObj = document.getElementById('tdCost')
+    document.getElementById('btnReset').onclick = resetInputs;
+    document.getElementById('btnCalcCost').onclick = calcCost;
 }
-            function resetInputs() {
-                lengthObj.value = '';
-                girthObj.value = '';
-                weightObj.innerHTML = '';
+function resetInputs() {
+    lengthObj.value = '';
+    heightObj.value = '';
+    widthObj.value = '';
+	costObj.innerHTML = '';
 }
-            function calcWeight() {
-                var length = new Number(lengthObj.value);
-                var girth = new Number(girthObj.value);
-                weightObj.innerHTML = '';
-                if(isNaN(length) || isNaN(girth)) {
-                    alert('Invalid length or girth');
-                    return;
-                }
-                weightObj.innerHTML = length*Math.pow(girth,2)/800;
+function calcCost() {
+    var length = new Number(lengthObj.value);
+    var width = new Number(widthObj.value);
+	var height = new Number(heightObj.value);
+    costObj.innerHTML = '';
+    if(isNaN(length) || isNaN(height) || isNaN(width)) {
+        alert('Invalid length or height');
+        return;
+	}
+    var glassCost=(((length*width*2)+(width*height*2)+(length*height*2))*0.06);
+	var glueCost=((height*4)+(length*2)+(width*2))*0.1; 
+	var labour=(60/6000)*((height*4)+(length*2)+(width*2));
+    var GST=(glassCost+glueCost+labour)*0.1;
+    costObj.innerHTML = glassCost+glueCost+labour+GST;
 }
